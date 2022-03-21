1afe45f72a1e0cc002628b6a201fc3ec27fa4c46
Graeme Montrose <teentrose@gmail.com>
2022-03-20 19:22:59 -0600

    added switch statement

--- db/schema.sql
+++ db/schema.sql
@@ -6,19 +6,19 @@ USE company_db;
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
-)
+);
 
-CREATE TABLE role(
+CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id VARCHAR(30)
-)
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT references department(id)
+);
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
-)
+    role_id INT references roles(id),
+    manager_id INT references employee(id) on delete set null
+    );
--- server.js
+++ server.js
@@ -7,44 +7,47 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'Socrani45@?',
-      database: 'company_db',
-    },
-    console.log(`Connected to the company_db database.`)
-  );
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Socrani45@?',
+    database: 'company_db',
+  })
+db.connect(function (err) {
+  if (err) throw err;
+  console.log(`Connected to the company_db database.`)
+   startmenu()
+})
 
-const interfaceQuestion = {
+const interfaceQuestion = [{
   type: "list",
   message: "What would you like to do?",
   name: "choice",
-  choices: ["View All Employees", 
-  "Add Employee", 
-  "Update Employee Role",
-  "View All Roles",
-  "Add Role",
-  "View All Departments",
-  "Add Department",
-  "Quit"]
-}
+  choices: ["View All Employees",
+    "Add Employee",
+    "Update Employee Role",
+    "View All Roles",
+    "Add Role",
+    "View All Departments",
+    "Add Department",
+    "Quit"]
+}]
 const addDepartmentQuestion = {
-    type: "input",
-    message: "What is this new department called?",
-    name: "DeptName"
-  }
+  type: "input",
+  message: "What is this new department called?",
+  name: "DeptName"
+}
 
 const addRoleQuestions = [
   {
     type: "input",
     message: "What is this new role called?",
     name: "RoleName"
-  },{
+  }, {
     type: "input",
     message: "What is the new role's salary?",
     name: "RoleSalary"
-  },{
+  }, {
     type: "list",
     message: "What department is this role in?",
     name: "RoleDept",
@@ -56,19 +59,50 @@ const addEmployeeQuestions = [
     type: "input",
     message: "What is the new employee's first name?",
     name: "firstName"
-  },{
+  }, {
     type: "input",
     message: "What is the new employee's last name?",
     name: "lastName"
-  },{
+  }, {
     type: "list",
     message: "What is their role?",
     name: "empRole",
     // choices: [Link the database]
-  },{
+  }, {
     type: "list",
     message: "Who is their manager?",
     name: "empManager",
     // choices: [, "No Manager"]
   }
 ]
+function startmenu(){
+  inquirer.prompt(interfaceQuestion)
+  .then(function(response){
+    switch(response.choice){
+      case "View All Employees":
+        ViewAllEmployees()
+        break;
+      case "Add Employee":
+        addEmployee()
+        break;
+      case "Update Employee Role":
+        updateEmployeeRole()
+        break;
+      case "View All Roles":
+        viewAllRoles()
+        break;
+        case "Add Role":
+          addRole()
+          break;
+      case "View All Departments":
+        viewAllDepts()
+        break;
+      case "Add Department":
+        addDept()
+        break;
+      default:
+        db.end()
+        process.exit(0)
+    }
+  })
+}
